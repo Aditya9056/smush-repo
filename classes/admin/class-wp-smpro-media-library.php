70eb892f633f0e4eeee406641e0957dd8811fa42
Saurabh Shukla <contact.saurabhshukla@gmail.com>
2014-08-10 13:30:16 +0530

    split custom column method added documentation for media-library class

--- classes/admin/class-wp-smpro-media-library.php
+++ classes/admin/class-wp-smpro-media-library.php
@@ -17,14 +17,19 @@ if (!class_exists('WpSmProMediaLibrary')) {
 	 * 
 	 */
 	class WpSmProMediaLibrary {
-
+                
+                /**
+                 * Constructor
+                 */
 		public function __construct() {
-
+                        
+                        // get the DEV api key
 			$wpmudev_apikey = get_site_option( 'wpmudev_apikey' );
 
 			// add the admin option screens
 			add_action( 'admin_init', array( $this, 'admin_init' ) );
-
+                        
+                        // if there's a key
 			if ( ! empty( $wpmudev_apikey ) ) {
 				// add extra columns for smushing to media lists
 				add_filter( 'manage_media_columns', array( $this, 'columns' ) );
@@ -33,7 +38,7 @@ if (!class_exists('WpSmProMediaLibrary')) {
 			
 		}
 		
-				/**
+                /**
 		 * Print column header for Smush.it results in the media library
 		 * 
 		 * @param array $defaults The default columns
@@ -50,6 +55,7 @@ if (!class_exists('WpSmProMediaLibrary')) {
 		 * 
 		 * @param string $column_name The name of the column
 		 * @param int $id The attachment id
+                 * @return null
 		 */
 		function custom_column( $column_name, $id ) {
 
@@ -57,48 +63,78 @@ if (!class_exists('WpSmProMediaLibrary')) {
 			if ( 'smushit' != $column_name ) {
 				return;
 			}
-
-			$attachment_file_path = get_attached_file($id);
-
+                        
 			// otherwise, get the smush meta
 			$smush_meta_full = get_post_meta( $id, 'smush_meta_full', true );
 			
-			// if there's smush details, show it
+			// if the image is smushed
 			if ( ! empty( $smush_meta_full ) ) {
-				?>
-				<p class="smush-status">
-					<?php echo $smush_meta_full['status_msg']; ?>
-				</p>
-                                <?php
-                                $button_show=$this->re_smush_button($smush_meta_full);
-                                if($button_show===true){
-                                ?>
-                                        <button class="wp-smpro-smush button">
-                                                <span>
-                                                        <?php _e('Re-smush', WP_SMPRO_DOMAIN); ?>
-                                                </span>
-                                        </button>
-                                <?php
-                                }
+                                
+                                // the status
+				$status_txt = $smush_meta_full['status_msg']; 
+                                
+                                // check if we need to show the resmush button
+                                $show_button = $this->show_resmush_button($smush_meta_full);
+                                
+                                // the button text
+                                $button_txt = __('Re-smush', WP_SMPRO_DOMAIN);
+                                
 			} else {
-				// not smushed yet, check if attachment is image
-				if ( wp_attachment_is_image( $id ) ) {
-					?>
-					<p class="smush-status">
-						<?php _e( 'Not processed', WP_SMPRO_DOMAIN ); ?>
-					</p>
-					<button class="wp-smpro-smush button">
-						<span>
-							<?php _e('Smush.it now!', WP_SMPRO_DOMAIN); ?>
-						</span>
-					</button>
-				<?php
-				}
+				
+                                // the status
+                                $status_txt = __( 'Not processed', WP_SMPRO_DOMAIN );; 
+
+                                // we need to show the smush button
+                                $show_button = true;
+
+                                // the button text
+                                $button_txt = __('Smush.it now!', WP_SMPRO_DOMAIN);
+				
 			}
+                        
+                        $this->column_html($status_txt, $button_txt, $show_button);
 			
 		}
                 
-                function re_smush_button($smush_meta_full){
+                /**
+                 * Print the column html
+                 * 
+                 * @param string $status_txt Status text
+                 * @param string $button_txt Button label
+                 * @param boolean $show_button Whether to shoe the button
+                 * @return null
+                 */
+                function column_html( $status_txt= '', $button_txt = '', $show_button=true ){
+                        // don't proceed if attachment is image
+                        if ( wp_attachment_is_image( $id ) ) {
+                                return;
+                        }
+                        ?>
+                        <p class="smush-status">
+                                <?php echo $status_txt; ?>
+                        </p>
+                        <?php
+                        // if we aren't showing the button
+                        if(!$show_button){
+                                return;
+                        }
+                        ?>
+                        <button class="wp-smpro-smush button">
+                                <span>
+                                        <?php echo $button_txt; ?>
+                                </span>
+                        </button>
+                        <?php
+                        
+                }
+                
+                /**
+                 * Whether to show resmush buton
+                 * 
+                 * @param array $smush_meta_full the smush metadata for full image size
+                 * @return boolean true to display the button
+                 */
+                function show_resmush_button($smush_meta_full){
                         $button_show = false;
                         
                         $status = (int)$smush_meta_full['status_code'];
