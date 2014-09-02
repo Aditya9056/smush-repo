e839ef760c34dff0a633efa71f19999e348fd127
Saurabh Shukla <contact.saurabhshukla@gmail.com>
2014-09-02 11:57:58 +0530

    fixed admin notice

--- classes/admin/class-wp-smpro-admin.php
+++ classes/admin/class-wp-smpro-admin.php
@@ -175,7 +175,7 @@ if (!class_exists('WpSmProAdmin')) {
                         if (boolval(get_option(WP_SMPRO_PREFIX . 'bulk-received', 0))) {
                                 $message = array();
                                 $message[] = sprintf(__('A recent bulk smushing request on your site %s has been completed!', WP_SMPRO_DOMAIN), get_option('siteurl'));
-                                $message[] = sprintf(__('Visit your dashboard (%s) to download the smushed images to your site.', WP_SMPRO_DOMAIN), admin_url('upload.php?page=wp-smpro-admin'));
+                                $message[] = sprintf(__('Visit <a href="%s">Dashboard &raquo; WP Smush.it Pro</a> to download the smushed images to your site.', WP_SMPRO_DOMAIN), admin_url('upload.php?page=wp-smpro-admin'));
                                 ?>
                                 <div class="updated">
                                         <p>
