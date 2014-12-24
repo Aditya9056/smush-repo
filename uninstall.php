155f63ad4b7906b87758fd6cd23bb64f54ebcc0c
Umesh Kumar <umeshsingla05@gmail.com>
2014-12-24 12:27:29 +0530

    delete plugin data on uninstall

--- uninstall.php
+++ uninstall.php
@@ -12,17 +12,24 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 }
 global $wpdb;
 $smush_pro_keys = array(
+	'auto',
+	'remove_meta',
+	'progressive',
+	'debug_mode',
+	'hide-notice',
 	'sent-ids',
 	'bulk-sent',
 	'bulk-received',
 	'current-requests',
 );
 foreach ( $smush_pro_keys as $key ) {
+	$key = 'wp-smpro-' . $key;
 	if ( is_multisite() ) {
 		$blogs = $wpdb->get_results( "SELECT blog_id FROM {$wpdb->blogs}", ARRAY_A );
 		if ( $blogs ) {
 			foreach ( $blogs as $blog ) {
 				switch_to_blog( $blog['blog_id'] );
+				delete_option( $key );
 				delete_site_option( $key );
 			}
 			restore_current_blog();
