407a92b152361c7607abad14e335155ded3cdf68
Umesh Kumar <umeshsingla05@gmail.com>
2014-12-24 14:35:57 +0530

    on uninstall send a reset request to API

--- uninstall.php
+++ uninstall.php
@@ -10,6 +10,29 @@
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit();
 }
+//Check if there is a pending bulk request, tell api to remove it
+$bulk_request  = get_option(  "wp-smpro-bulk-sent", array() );
+$current_requests = get_option( "wp-smpro-current-requests", array() );
+if( !empty( $bulk_request) && !empty( $current_requests[$bulk_request]) ) {
+	$request_data               = array();
+	$request_data['api_key']    = get_site_option( 'wpmudev_apikey' );
+	$request_data['token']      = $current_requests[$bulk_request]['token'];
+	$request_data['request_id'] = $bulk_request;
+
+	$request_data = json_encode( $request_data );
+
+	$req_args = array(
+		'body'       => array(
+			'json' => $request_data
+		),
+		'user-agent' => 'WP Smush PRO/' . WP_SMPRO_VERSION . '(' . '+' . get_site_url() . ')',
+		'timeout'    => 30,
+		'sslverify'  => false
+	);
+
+// make the post request and return the response
+	wp_remote_post( 'https://smush.wpmudev.org/reset/', $req_args );
+}
 global $wpdb;
 $smush_pro_keys = array(
 	'auto',
