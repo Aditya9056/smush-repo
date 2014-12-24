fea360ff553376373815d0cdaa67b60517273742
Umesh Kumar <umeshsingla05@gmail.com>
2014-12-24 11:55:34 +0530

    update reset link style

--- uninstall.php
+++ uninstall.php
@@ -11,32 +11,14 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit();
 }
 global $wpdb;
-//Network_options
-$wdfb_option_keys = array(
-	'wdfb_api',
-	'wdfb_autopost',
-	'wdfb_widget_pack',
-	'wdfb_opengraph',
-	'wdfb_comments',
-	'wdfb_grant',
-	'wdfb_connect',
-	'widget_wdfb_widgetactivityfeed',
-	'widget_wdfb_widgetalbums',
-	'widget_wdfb_widgetconnect',
-	'widget_wdfb_widgetevents',
-	'widget_wdfb_widgetfacepile',
-	'widget_wdfb_widgetlikebox',
-	'widget_wdfb_widgetrecentcomments',
-	'widget_wdfb_widgetrecommendations',
-	'wdfb_button',
-	'wdfb_groups',
-	'wdfb_error_log',
-	'wdfb_notice_log'
+$smush_pro_keys = array(
+	'sent-ids',
+	'bulk-sent',
+	'bulk-received',
+	'current-requests',
 );
-//Delete options
-foreach ( $wdfb_option_keys as $key ) {
+foreach ( $smush_pro_keys as $key ) {
 	if ( is_multisite() ) {
-		global $wpdb;
 		$blogs = $wpdb->get_results( "SELECT blog_id FROM {$wpdb->blogs}", ARRAY_A );
 		if ( $blogs ) {
 			foreach ( $blogs as $blog ) {
@@ -49,8 +31,6 @@ foreach ( $wdfb_option_keys as $key ) {
 		delete_option( $key );
 	}
 }
-//delete from site meta
-
-//Remove Cron Job
-wp_clear_scheduled_hook( 'wdfb_import_comments' );
+//Delete post meta for all the images
+$wpdb->query( "DELETE FROM $wpdb->postmeta WHERE meta_key='wp-smpro-is-smushed' OR meta_key='wp-smpro-smush-data' OR meta_key LIKE '%wp-smpro-request%' " );
 ?>
