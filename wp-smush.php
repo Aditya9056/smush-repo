485b502df1f40b877325d34f09dd0bbd6c08e2a5
Umesh Kumar <umeshsingla05@gmail.com>
2015-09-15 11:56:58 +0530

    Added email campaign

--- wp-smush.php
+++ wp-smush.php
@@ -856,15 +856,50 @@ if ( ! class_exists( 'WpSmush' ) ) {
 //Include Admin classes
 require_once( WP_SMUSH_DIR . '/lib/class-wp-smush-bulk.php' );
 require_once( WP_SMUSH_DIR . '/lib/class-wp-smush-admin.php' );
-//include_once( WP_SMUSH_DIR . '/extras/dash-notice/wpmudev-dash-notification.php' );
 
-//register items for the dashboard plugin
-global $wpmudev_notices;
-$wpmudev_notices[] = array(
-	'id'      => 912164,
-	'name'    => 'WP Smush Pro',
-	'screens' => array(
-		'media_page_wp-smush-bulk',
-		'upload'
-	)
+//require_once( WP_SMUSH_DIR . '/extras/dash-notice/wpmudev-dash-notification.php' );
+require_once( WP_SMUSH_DIR . '/extras/free-dashboard/module.php' );
+
+// Register the current plugin.
+do_action(
+	'wdev-register-plugin',
+	/* 1             Plugin ID */ plugin_basename( __FILE__ ), /* Plugin ID */
+	/* 2          Plugin Title */ 'WP Smush',
+	/* 3 https://wordpress.org */ '/plugins/wp-smushit/',
+	/* 4      Email Button CTA */ '',
+	/* 5  getdrip Plugin param */ ''
 );
+
+// The rating message contains 2 variables: user-name, plugin-name
+add_filter(
+	'wdev-rating-message-' . plugin_basename( __FILE__ ),
+	'wp_smush_rating_message'
+);
+
+/**
+ * Filters the rating message, include stats if greater than 1Mb
+ * @param $message
+ *
+ * @return string
+ */
+function wp_smush_rating_message( $message ) {
+	global $wpsmushit_admin;
+	$savings     = $wpsmushit_admin->global_stats();
+	$image_count = $wpsmushit_admin->total_count();
+	$show_stats = false;
+
+	//If there is any saving, greater than 1Mb, show stats
+	if( !empty( $savings ) && !empty( $savings['bytes'] ) && $savings['bytes'] > 1048576 ) {
+		$show_stats = true;
+	}
+
+	$message = "Hey %s, you've been using %s for a while now, and we hope you're happy with it.";
+
+	//Conditionally Show stats in rating message
+	if( $show_stats ) {
+		$message .= sprintf( " You've smushed <strong>%s</strong> from %d images already, improving the speed and SEO ranking of this site!", $savings['human'], $image_count );
+	}
+	$message .= " We've spent countless hours developing this free plugin for you, and we would really appreciate it if you dropped us a quick rating!";
+
+	return $message;
+}// All done!
