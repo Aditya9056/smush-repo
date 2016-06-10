ab52cf2bd92eb419aa0d6435256c9c90a7eeb29e
Umesh Kumar <umeshsingla05@gmail.com>
2016-06-10 15:15:36 +0530

    pngjpg: Convert image sizes

--- lib/class-wp-smush-png_jpg.php
+++ lib/class-wp-smush-png_jpg.php
@@ -142,15 +142,23 @@ if ( ! class_exists( 'WpSmushPngtoJpg' ) ) {
 
 		/**
 		 * Update the image URL, MIME Type, Attached File, file path in Meta, URL in post content
+		 *
 		 * @param $id Attachment ID
 		 * @param $o_file Original File Path
 		 * @param $n_file New File Path
 		 * @param $meta Attachment Meta
+		 * @param $size_k Image Size
 		 *
 		 * @return mixed Attachment Meta with updated file path
 		 *
 		 */
-		function update_image_path( $id, $o_file, $n_file, $meta ) {
+		function update_image_path( $id, $o_file, $n_file, $meta, $size_k ) {
+
+			//Upload Directory
+			$upload_dir = wp_upload_dir();
+
+			//Upload Path
+			$upload_path = trailingslashit( $upload_dir['basedir'] );
 
 			//Get the File name without ext
 			$new_file = substr( $o_file, 0, - 3 );
@@ -159,29 +167,46 @@ if ( ! class_exists( 'WpSmushPngtoJpg' ) ) {
 			$new_ext = substr( $n_file, - 3 );
 
 			//Updated File name
-			$new_file = $new_file.$new_ext;
+			$new_file = $new_file . $new_ext;
 
+			//Current URL for image
 			$o_url = wp_get_attachment_url( $id );
 
-			//Update File path, Attached File, GUID
-			$meta = empty( $meta ) ? wp_get_attachment_metadata( $id ) :  $meta;
-
-			if( ! empty(  $meta )) {
-				$meta['file'] = $new_file;
+			//Update URL for image size
+			if( 'full' != $size_k ) {
+				$base_url = dirname( $o_url );
+				$o_url    = $base_url . '/' . basename( $o_file );
 			}
 
-			//Update Attached File
-			update_attached_file( $id, $meta['file'] );
+			//Update File path, Attached File, GUID
+			$meta = empty( $meta ) ? wp_get_attachment_metadata( $id ) : $meta;
 
-			//Update Mime type
-			wp_update_post( array(
-					'ID' => $id,
-					'post_mime_type' => 'image/jpg'
-				)
-			);
+			//Update File Path, Attached file, Mime Type for Image
+			if ( 'full' == $size_k ) {
+				if( ! empty( $meta ) ) {
+					$new_file = str_replace( $upload_path, '', $new_file );
+					$meta['file'] = $new_file;
+				}
+				//Update Attached File
+				update_attached_file( $id, $meta['file'] );
 
-			//Get the updated image URL
-			$n_url = wp_get_attachment_url( $id );
+				//Update Mime type
+				wp_update_post( array(
+						'ID'             => $id,
+						'post_mime_type' => 'image/jpg'
+					)
+				);
+			}else{
+				$meta['sizes'][$size_k]['file'] = basename( $new_file ) ;
+				$meta['sizes'][$size_k]['mime-type'] = 'image/jpg';
+			}
+
+			if( 'full' == $size_k ) {
+				//Get the updated image URL
+				$n_url = wp_get_attachment_url( $id );
+			}else{
+				$n_url = dirname( $o_url ) . '/' . basename( $new_file );
+			}
 
 			//Update In Post Content
 			global $wpdb;
@@ -200,22 +225,36 @@ if ( ! class_exists( 'WpSmushPngtoJpg' ) ) {
 
 		/**
 		 * Perform the conversion process, using WordPress Image Editor API
+		 *
 		 * @param $id Attachment Id
 		 * @param $file Attachment File path
 		 * @param $meta Attachment meta
+		 * @param $size Image size, default empty for full image
+		 *
+		 * @return array
 		 */
-		function convert_to_jpg( $id = '', $file = '', $meta = '', $size = '' ) {
+		function convert_to_jpg( $id = '', $file = '', $meta = '', $size = 'full' ) {
+
+			$result = array(
+				'meta'    => $meta,
+				'savings' => ''
+			);
+
+			//Flag: Whether the image was converted or not
+			if ( 'full' == $size ) {
+				$result['converted'] = false;
+			}
 
 			//If any of the values is not set
 			if ( empty( $id ) || empty( $file ) || empty( $meta ) ) {
-				return $meta;
+				return $result;
 			}
 
 			$editor = wp_get_image_editor( $file );
 
 			if ( is_wp_error( $editor ) ) {
 				//Use custom method maybe
-				return;
+				return $result;
 			}
 
 			//Save PNG as JPG
@@ -223,7 +262,7 @@ if ( ! class_exists( 'WpSmushPngtoJpg' ) ) {
 
 			//If image editor was unable to save the image, return
 			if ( is_wp_error( $new_image_info ) ) {
-				return;
+				return $result;
 			}
 
 			//Get the file size of original image
@@ -237,17 +276,31 @@ if ( ! class_exists( 'WpSmushPngtoJpg' ) ) {
 				//Delete the JPG image and return
 				@unlink( $n_file );
 
-				return;
+				return $result;
 			}
 
 			//Get the savings
 			$savings = $o_file_size - $n_file_size;
 
+			//@todo: Update the code below as per the image size
 			//Update the File Details
-			$this->update_image_path( $id, $file, $n_file, $meta );
+			$meta = $this->update_image_path( $id, $file, $n_file, $meta, $size );
 
 			//Store Stats
-			$this->update_stats( $savings );
+			$savings = array(
+				'bytes'       => $savings,
+				'size_before' => $o_file_size,
+				'size_after'  => $n_file_size
+			);
+
+			$result['meta']    = $meta;
+			$result['savings'] = $savings;
+
+			if ( 'full' == $size ) {
+				$result['converted'] = true;
+			}
+
+			return $result;
 		}
 
 		/**
@@ -287,16 +340,47 @@ if ( ! class_exists( 'WpSmushPngtoJpg' ) ) {
 			 *
 			 * @param string $file File path for the image
 			 *
+			 * @param string $size Image size beign converted
+			 *
 			 */
-			if ( ! $should_convert = apply_filters( 'wp_smush_convert_to_jpg', $should_convert, $id, $file ) ) {
+			if ( ! $should_convert = apply_filters( 'wp_smush_convert_to_jpg', $should_convert, $id, $file, 'full' ) ) {
 				return $meta;
 			}
 
-			$meta = $this->convert_to_jpg( $id, $file, $meta );
+			//Perform the conversion, and update path
+			$result = $this->convert_to_jpg( $id, $file, $meta );
 
-			//Resize all other image sizes
-			//@todo:
-			return $meta;
+			$savings = $result['savings'];
+
+			//If original image was converted and other sizes are there for the image, Convert all other image sizes
+			if ( $result['converted'] ) {
+				if ( ! empty( $meta['sizes'] ) ) {
+					foreach ( $meta['sizes'] as $size_k => $data ) {
+						//Perform the conversion
+						if ( ! $should_convert = apply_filters( 'wp_smush_convert_to_jpg', $should_convert, $id, $file, $size_k ) ) {
+							continue;
+						}
+						$s_file = path_join( dirname( $file ), $data['file'] );
+
+						//Perform the conversion, and update path
+						$result = $this->convert_to_jpg( $id, $s_file, $result['meta'], $size_k );
+
+						//Add all the stats
+						array_walk_recursive($result['savings'], function($item, $key) use (&$savings){
+							$savings[$key] = isset($savings[$key]) ?  $item + $savings[$key] : $item;
+						});
+					}
+				}
+
+				//Update the Final Stats
+
+				/**
+				 * Do action, if the PNG to JPG conversion was successful
+				 */
+				do_action( 'wp_smush_png_jpg_converted', $id, $meta, $savings );
+			}
+
+			return $result['meta'];
 
 		}
 	}
