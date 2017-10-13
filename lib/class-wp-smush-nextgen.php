3b847e9b5ccc28ffe1349a223772772b02edd20a
Umesh Kumar <umeshsingla05@gmail.com>
2017-10-13 14:37:49 +0530

    NextGen Integration: Fixed error causing problem on Nexgen gallery creation

--- lib/class-wp-smush-nextgen.php
+++ lib/class-wp-smush-nextgen.php
@@ -31,15 +31,20 @@ if ( ! class_exists( 'WpSmushNextGen' ) ) {
 		}
 
 		function init() {
-			global $WpSmush;
+			global $WpSmush, $wpsmush_settings;
 			//Filters the setting variable to add S3 setting title and description
 			add_filter( 'wp_smush_settings', array( $this, 'register' ), 5 );
 
 			//Filters the setting variable to add S3 setting in premium features
 			add_filter( 'wp_smush_pro_settings', array( $this, 'add_setting' ), 5 );
 
-			//return if not a pro user
-			if( !$WpSmush->validate_install() ) {
+			//Check if integration is Enabled or not
+			//Smush NextGen key
+			$opt_nextgen     = WP_SMUSH_PREFIX . 'nextgen';
+			$opt_nextgen_val = $wpsmush_settings->get_setting( $opt_nextgen, false );
+
+			//return if not a pro user, or nextgen integration is not enabled
+			if( !$WpSmush->validate_install() || !$opt_nextgen_val ) {
 				return;
 			}
 
