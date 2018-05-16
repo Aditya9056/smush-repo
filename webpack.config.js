3b589a1e3667119ac6ede7b1ae50c9b3fa873249
Joel James <me@joelsays.com>
2018-05-16 11:00:03 +0530

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,35 +3,22 @@ const path = require( 'path' );
 const webpack = require( 'webpack' );
 
 const config = {
-	source: {},
 	output: {}
 };
 
-// Full path of main files that need to be ran through the bundler.
-config.source.adminscss = './_src/scss/admin.scss';
-config.source.resizescss = './_src/scss/resize-detection.scss';
-config.source.adminjs = './_src/js/admin.js';
-config.source.resizejs = './_src/js/resize-detection.js';
-
-// Path where the scss & js should be compiled to.
-config.output.scssDirectory = 'assets/shared-ui-2/css';       // No trailing slash.
-config.output.jsDirectory = 'assets/shared-ui-2/js';        // No trailing slash.
-
-// File names of the compiled scss & js.
-config.output.adminScssFileName = 'admin.min.css';
-config.output.resizeScssFileName = 'resize-detection.min.css';
-config.output.adminJsFileName = 'admin.min.js';
-config.output.resizeJsFileName = 'resize-detection.min.js';
-
 // The path where the Shared UI fonts & images should be sent. (relative to config.output.jsFileName)
-config.output.imagesDirectory = '../images/';            // Trailing slash required.
-config.output.fontsDirectory = '../fonts/';             // Trailing slash required.
+config.output.imagesDirectory = '..//images'; // Trailing slash required.
+config.output.fontsDirectory = '../fonts'; // Trailing slash required.
 
-const adminScssConfig = {
-	entry: config.source.adminscss,
+const scssConfig = {
+	entry: {
+		'shared-ui': './_src/scss/shared-ui.scss',
+		'admin': './_src/scss/admin.scss',
+		'resize-detection': './_src/scss/resize-detection.scss'
+	},
 	output: {
-		filename: config.output.adminScssFileName,
-		path: path.resolve( __dirname, config.output.scssDirectory )
+		filename: '[name].min.css',
+		path: path.resolve( __dirname, 'assets/css' )
 	},
 	module: {
 		rules: [
@@ -82,74 +69,19 @@ const adminScssConfig = {
 		]
 	},
 	plugins: [
-		new ETP( config.output.adminScssFileName )
+		new ETP( '[name].min.css' )
 	]
 };
 
-const resizeScssConfig = {
-	entry: config.source.resizescss,
-	output: {
-		filename: config.output.resizeScssFileName,
-		path: path.resolve( __dirname, config.output.scssDirectory )
+const jsConfig = {
+	entry: {
+		'shared-ui': './_src/js/shared-ui.js',
+		'admin': './_src/js/admin-index.js',
+		'resize-detection': './_src/js/public-resize-detection.js'
 	},
-	module: {
-		rules: [
-			{
-				test: /\.scss$/,
-				exclude: /node_modules/,
-				use: ETP.extract( {
-					fallback: 'style-loader',
-					use: [
-						{
-							loader: 'css-loader'
-						},
-						{
-							loader: 'postcss-loader'
-						},
-						{
-							loader: 'resolve-url-loader'
-						},
-						{
-							loader: 'sass-loader',
-							options: {
-								sourceMap: true
-							}
-						}
-					]
-				} )
-			},
-			{
-				test: /\.(png|jpg|gif)$/,
-				use: {
-					loader: 'file-loader', // Instructs webpack to emit the required object as file and to return its public URL.
-					options: {
-						name: '[name].[ext]',
-						outputPath: config.output.imagesDirectory
-					}
-				}
-			},
-			{
-				test: /\.(woff|woff2|eot|ttf|otf|svg)$/,
-				use: {
-					loader: 'file-loader', // Instructs webpack to emit the required object as file and to return its public URL.
-					options: {
-						name: '[name].[ext]',
-						outputPath: config.output.fontsDirectory
-					}
-				}
-			}
-		]
-	},
-	plugins: [
-		new ETP( config.output.resizeScssFileName )
-	]
-};
-
-const adminJsConfig = {
-	entry: config.source.adminjs,
 	output: {
-		filename: config.output.adminJsFileName,
-		path: path.resolve( __dirname, config.output.jsDirectory )
+		filename: '[name].min.js',
+		path: path.resolve( __dirname, 'assets/js' )
 	},
 	module: {
 		rules: [
@@ -176,10 +108,10 @@ const adminJsConfig = {
 };
 
 const resizeJsConfig = {
-	entry: config.source.resizejs,
+	entry: './_src/js/public-resize-detection.js',
 	output: {
-		filename: config.output.resizeJsFileName,
-		path: path.resolve( __dirname, config.output.jsDirectory )
+		filename: 'resize-detection.min.js',
+		path: path.resolve( __dirname, 'assets/js' )
 	},
 	module: {
 		rules: [
@@ -197,4 +129,4 @@ const resizeJsConfig = {
 	},
 };
 
-module.exports = [adminScssConfig, resizeScssConfig, adminJsConfig, resizeJsConfig];
+module.exports = [scssConfig, jsConfig];
