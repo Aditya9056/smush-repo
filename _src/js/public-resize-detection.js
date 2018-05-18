3e5f97fb41590142d33a8430de298c5e8dcf4523
Joel James <me@joelsays.com>
2018-05-18 18:54:02 +0530

    Remove unwanted ajax from script

--- _src/js/public-resize-detection.js
+++ _src/js/public-resize-detection.js
@@ -16,8 +16,6 @@ jQuery( function ( $ ) {
 			revert_detection();
 		} );
 
-		$( 'body' ).on( 'click', '.smush-resize-submit', resize_image );
-
 	} );
 
 	/**
@@ -28,7 +26,7 @@ jQuery( function ( $ ) {
 	 */
 	var detect_wrong_imgs = function () {
 
-		$( 'body img[data-attachment-id]' ).each( function () {
+		$( 'body img[data-smush-image]' ).each( function () {
 
 			var ele = $( this );
 
@@ -45,16 +43,10 @@ jQuery( function ( $ ) {
 				higher_width = ( css_width * 1.5 ) < img_width,
 				higher_height = ( css_height * 1.5 ) < img_height,
 				smaller_width = css_width > img_width,
-				smaller_height = css_height > img_height,
-				attachment_id = ele.data( 'attachment-id' );
+				smaller_height = css_height > img_height;
 
 			// Incase image is in correct size, do not continue.
 			if ( !higher_width && !higher_height && !smaller_width && !smaller_height ) {
-				return true;
-			}
-
-			// Continue only if image has a valid attachment id.
-			if ( attachment_id === '' || attachment_id <= 0 ) {
 				return true;
 			}
 
@@ -72,7 +64,6 @@ jQuery( function ( $ ) {
 				'<span class="smush-tag">' + img_width + ' × ' + img_height + 'px</span>' +
 				'<i class="smush-front-icons smush-front-icon-arrows-in" aria-hidden="true"></i>' +
 				'<span class="smush-tag smush-tag-success">' + css_width + ' × ' + css_height + 'px</span>' +
-				'<i class="smush-front-icons smush-front-icon-arrow-right smush-resize-submit" data-attachment-id="' + attachment_id + '" aria-hidden="true"></i>' +
 				'</div>';
 
 			var after_content = '';
@@ -99,35 +90,5 @@ jQuery( function ( $ ) {
 
 		// Remove custom class from images.
 		$( '.smush-detected-img' ).removeClass( 'smush-detected-img' );
-	};
-
-	/**
-	 * Function to resize image via CDN.
-	 *
-	 * Send ajax request and set new resized version
-	 * based on the required container size.
-	 */
-	var resize_image = function () {
-
-		var ele = $( this );
-
-		ele.addClass( 'smush-front-icon-loader' );
-
-		// Send a ajax request to set new resized version.
-		var params = {
-			action: 'smush_auto_resize',
-			attachment_id: ele.data( 'attachment-id' ),
-			resize_nonce: wp_smush_resize_vars.ajax_nonce,
-		};
-
-		// Set the new resized version of image.
-		$.get( wp_smush_resize_vars.ajaxurl, params, function ( res ) {
-
-			ele.removeClass( 'smush-front-icon-loader' );
-
-		} ).done( function ( res ) {
-
-			ele.removeClass( 'smush-front-icon-loader' );
-		} );
 	};
 } );
