e98fe0d554fe66d1191dd5db4b3b5434f23014d9
Umesh Kumar <umeshsingla05@gmail.com>
2018-05-24 10:56:51 +0530

    Added privacy policy content, Updated readme, pot file

--- lib/class-wp-smush.php
+++ lib/class-wp-smush.php
@@ -155,6 +155,11 @@ if ( ! class_exists( 'WpSmush' ) ) {
 			//Register Function for sending unsmushed image count to hub
 			add_filter( 'wdp_register_hub_action', array( $this, 'smush_stats' ) );
 
+			/**
+			 * Add information to privacy policy page (only during creation).
+			 */
+			add_filter( 'wp_get_default_privacy_policy_content', array( $this, 'add_policy' ) );
+
 		}
 
 		/**
@@ -2526,6 +2531,28 @@ if ( ! class_exists( 'WpSmush' ) ) {
 				$wpsmush_helper->drop_index( $wpdb->prefix. 'smush_dir_images', 'path' );
 				update_option( 'smush-directory-path-hash-updated', 1 );
 			}
+		}
+
+		/**
+		 * Add Smush Policy to "Privace Policy" page during creation.
+		 *
+		 * @since 2.3.0
+		 */
+		public function add_policy( $content ) {
+			$dir_path = get_plugin_dir();
+
+			$content .= '<h3>' . __( 'Plugin: Smush', 'wp-smushit' ) . '</h3>';
+			$content .=
+				'<p>'.__( 'Note: Smush does not interact with end users on your website. The only input option Smush has is to a newsletter subscription for site admins only. If you would like to notify your users of this in your privacy policy, you can use the information below.', 'wp-smushit' ) . '</p>';
+			$content .=
+				'<p>'. __( 'Smush sends images to the WPMU DEV servers to optimize them for web use. This includes the transfer of EXIF data. The EXIF data will either be stripped or returned as it is. It is not stored on the WPMU DEV servers.', 'wp-smushit' ) . '</p>';
+
+			if ( strpos( $dir_path, 'wp-smushit' ) !== false ) {
+				//Only for wordpress.org members
+				$content .=
+					'<p>' . __( 'Smush uses a third-party email service (Drip) to send informational emails to the site administrator. The administrator\'s email address is sent to Drip and a cookie is set by the service. Only administrator information is collected by Drip.', 'wp-smushit' ) . '</p>';
+			}
+			return $content;
 		}
 
 	}
