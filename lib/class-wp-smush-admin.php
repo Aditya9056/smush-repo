dd579b50f2f3431a9ac5f57cd210be082b22add4
Anton Vanyukov <anton@incsub.com>
2018-06-18 09:12:21 +0300

    Remove old shared-ui folders and dependencies

--- lib/class-wp-smush-admin.php
+++ lib/class-wp-smush-admin.php
@@ -12,11 +12,6 @@
 //Include Bulk UI
 require_once WP_SMUSH_DIR . 'lib/class-wp-smush-ui.php';
 
-//Load Shared UI
-if ( ! class_exists( 'WDEV_Plugin_Ui' ) ) {
-	//require_once WP_SMUSH_DIR . 'assets/shared-ui/plugin-ui.php';
-}
-
 if ( ! class_exists( 'WpSmushitAdmin' ) ) {
 	/**
 	 * Show settings in Media settings and add column to media library
