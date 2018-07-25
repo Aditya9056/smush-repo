1922afc50ffde19fbd8ce4a48dbd9a2d67e2fb57
Anton Vanyukov <anton@incsub.com>
2018-07-25 14:28:21 +0000

    Fix download script

--- misc/deploy-wordpress.sh
+++ misc/deploy-wordpress.sh
@@ -10,16 +10,6 @@ download() {
     fi
 }
 
-# http serves a single offer, whereas https serves multiple. we only want one
-download http://api.wordpress.org/core/version-check/1.7/ /tmp/wp-latest.json
-grep '[0-9]+\.[0-9]+(\.[0-9]+)?' /tmp/wp-latest.json
-LATEST_VERSION=$(grep -o '"version":"[^"]*' /tmp/wp-latest.json | sed 's/"version":"//')
-if [[ -z "$LATEST_VERSION" ]]; then
-    echo "Latest WordPress version could not be found"
-    exit 1
-fi
-WP_TESTS_TAG="tags/$LATEST_VERSION"
-
 set -ex
 
 install_wp() {
@@ -29,13 +19,7 @@ install_wp() {
 
 	mkdir -p $WP_CORE_DIR
 
-	if [ $WP_VERSION == 'latest' ]; then
-		local ARCHIVE_NAME='latest'
-	else
-		local ARCHIVE_NAME="wordpress-$WP_VERSION"
-	fi
-
-	download https://wordpress.org/${ARCHIVE_NAME}.tar.gz  /tmp/wordpress.tar.gz
+	download https://wordpress.org/latest.tar.gz  /tmp/wordpress.tar.gz
 	tar --strip-components=1 -zxmf /tmp/wordpress.tar.gz -C $WP_CORE_DIR
 }
 
