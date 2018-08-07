8d2312a2ef828a6a1fff115fc3877da4b781f2a3
Anton Vanyukov <anton@incsub.com>
2018-08-07 10:40:35 +0000

    Update codeception configs. Add first acceptance test. Add sql dump

---
+++ tests/acceptance/BulkSmushCest.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class BulkSmushCest
+ *
+ * Test Bulk Smush functionality
+ *
+ * @package AcceptanceTests
+ */
+
+/**
+ * Class BulkSmushCest
+ */
+class BulkSmushCest {
+	public function _before(AcceptanceTester $I) {}
+
+	public function _after(AcceptanceTester $I) {}
+
+	// Tests.
+	public function tryToTest(AcceptanceTester $I) {}
+}
