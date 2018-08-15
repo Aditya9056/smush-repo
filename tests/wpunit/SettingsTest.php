27a7a8158477e4f782686ad712b4947e678d97de
Joel James <me@joelsays.com>
2018-08-15 19:20:55 +0530

    Added unit test to skip and smush original images

--- tests/wpunit/SettingsTest.php
+++ tests/wpunit/SettingsTest.php
@@ -29,6 +29,24 @@ class SettingsTest extends \Codeception\TestCase\WPTestCase {
 	}
 
 	/**
+	 * Set Smush to Smush Pro.
+	 */
+	private function setPro() {
+		// Define test api key.
+		if ( ! defined( 'WPMUDEV_APIKEY' ) ) {
+			define( 'WPMUDEV_APIKEY', 'test_api_key' );
+		}
+
+		// Flag that api key is valid.
+		update_site_option( 'wp_smush_api_auth', array(
+			'test_api_key' => array(
+				'timestamp' => current_time( 'timestamp' ),
+				'validity'  => 'valid',
+			),
+		) );
+	}
+
+	/**
 	 * Test bulk limit for free users.
 	 */
 	public function testBulkLimit() {
@@ -65,4 +83,51 @@ class SettingsTest extends \Codeception\TestCase\WPTestCase {
 		$this->assertEquals( $expected, $wpsmush_settings );
 	}
 
+	/**
+	 * Test smushing original image.
+	 */
+	public function testSmushOriginal() {
+		global $wpsmushit_admin, $wpsmush_settings;
+
+		// Set Smush to Pro.
+		$this->setPro();
+
+		$file = dirname( dirname( __FILE__ ) ) . '/_data/images/image1.jpeg';
+
+		// Set smush original image setting to true.
+		$wpsmush_settings->settings['original'] = 1;
+
+		// Upload image and set meta data.
+		$id = $this->factory()->attachment->create_upload_object( $file );
+
+		// Get smush meta data for attachment.
+		$meta = get_post_meta( $id, $wpsmushit_admin->smushed_meta_key, true );
+
+		// Full size should be there in smushed sizes.
+		$this->assertTrue( isset( $meta['sizes']['full'] ) );
+	}
+
+	/**
+	 * Test skipping original image.
+	 */
+	public function testSkipOriginal() {
+		global $wpsmushit_admin, $wpsmush_settings;
+
+		// Set Smush to Pro.
+		$this->setPro();
+
+		$file = dirname( dirname( __FILE__ ) ) . '/_data/images/image1.jpeg';
+
+		// Set smush original image setting to false.
+		$wpsmush_settings->settings['original'] = 0;
+
+		// Upload image and set meta data.
+		$id = $this->factory()->attachment->create_upload_object( $file );
+
+		// Get smush meta data for attachment.
+		$meta = get_post_meta( $id, $wpsmushit_admin->smushed_meta_key, true );
+
+		// Full size should not be there in smushed sizes.
+		$this->assertFalse( isset( $meta['sizes']['full'] ) );
+	}
 }
