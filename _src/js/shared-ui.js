075e441441cf1aac9b82a842040725b1ace9de66
Anton Vanyukov <anton@incsub.com>
2018-10-15 08:56:32 +0000

    Fix mobile menus not working properly

--- _src/js/shared-ui.js
+++ _src/js/shared-ui.js
@@ -4,3 +4,4 @@
 require('@wpmudev/shared-ui/dist/js/_src/modals');
 require('@wpmudev/shared-ui/dist/js/_src/notifications');
 require('@wpmudev/shared-ui/dist/js/_src/scores');
+require('@wpmudev/shared-ui/dist/js/_src/select');
