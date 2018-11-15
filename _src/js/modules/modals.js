1bc02fe7a9ab08ece088cf7c62f1dde5b2a4b8d1
Anton Vanyukov <anton@incsub.com>
2018-11-15 13:59:42 +0300

    Add touch gesture support

--- _src/js/modules/modals.js
+++ _src/js/modules/modals.js
@@ -27,7 +27,12 @@
         },
         contentContainer: document.getElementById('smush-onboarding-content'),
         onboardingSlides: [ 'start', 'auto', 'lossy', 'strip_exif', 'original', 'usage' ],
+        touchX: null,
+        touchY: null,
 
+        /**
+         * Init module.
+         */
         init: function() {
             if ( ! this.modal ) {
                 return;
@@ -45,6 +50,51 @@
             }
         },
 
+        /**
+         * Get swipe coordinates.
+         *
+         * @param e
+         */
+        handleTouchStart: function(e) {
+            const firstTouch = e.touches[0];
+            this.touchX = firstTouch.clientX;
+            this.touchY = firstTouch.clientY;
+        },
+
+        /**
+         * Process swipe left/right.
+         *
+         * @param e
+         */
+        handleTouchMove: function(e) {
+            if ( ! this.touchX || ! this.touchY ) {
+                return;
+            }
+
+            const xUp = e.touches[0].clientX,
+                  yUp = e.touches[0].clientY,
+                  xDiff = this.touchX - xUp,
+                  yDiff = this.touchY - yUp;
+
+            if ( Math.abs(xDiff) > Math.abs(yDiff) ) {
+                if ( xDiff > 0 ) {
+                    if ( false === WP_Smush.onboarding.settings.last ) {
+                        WP_Smush.onboarding.next(null, 'next');
+                    }
+                } else {
+                    if ( false === WP_Smush.onboarding.settings.first ) {
+                        WP_Smush.onboarding.next(null, 'prev');
+                    }
+                }
+            }
+
+            this.touchX = null;
+            this.touchY = null;
+        },
+
+        /**
+         * Update the template, register new listeners.
+         */
         renderTemplate: function() {
             // Grab the selected value.
             const input = this.modal.querySelector('input[type="checkbox"]');
@@ -58,6 +108,9 @@
             if ( content ) {
                 this.contentContainer.innerHTML = content;
             }
+
+            this.modal.addEventListener('touchstart', this.handleTouchStart, false);
+            this.modal.addEventListener('touchmove', this.handleTouchMove, false);
 
             this.bindSubmit();
         },
@@ -94,10 +147,17 @@
          * Handle navigation.
          *
          * @param e
+         * @param whereTo
          */
-        next: function(e) {
-            const index = this.onboardingSlides.indexOf(this.settings.slide),
-                  newIndex = e.classList.contains('next') ? index + 1 : index - 1;
+        next: function(e, whereTo = null) {
+            const index = this.onboardingSlides.indexOf(this.settings.slide);
+            let newIndex = 0;
+
+            if ( ! whereTo ) {
+                newIndex = e.classList.contains('next') ? index + 1 : index - 1;
+            } else {
+                newIndex = 'next' === whereTo ? index + 1 : index - 1;
+            }
 
             this.settings = {
                 first: 0 === newIndex,
@@ -129,6 +189,11 @@
         }
     };
 
+    /**
+     * Template function (underscores based).
+     *
+     * @type {Function}
+     */
     WP_Smush.onboarding.template = _.memoize(id => {
         let compiled,
             options = {
