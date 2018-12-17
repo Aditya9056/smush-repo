db7a8edbf7fc607429a6f98aa6045594d415bc0a
calumbrash <calumbrash@gmail.com>
2018-12-18 11:25:42 +1300

    White Label - Hide hero image on CDN page.

--- app/views/meta-boxes/cdn/disabled-meta-box.php
+++ app/views/meta-boxes/cdn/disabled-meta-box.php
@@ -9,9 +9,11 @@
 ?>
 
 <div class="sui-block-content-center">
-	<img src="<?php echo esc_url( WP_SMUSH_URL . 'app/assets/images/graphic-smush-cdn-default.png' ); ?>"
-		srcset="<?php echo esc_url( WP_SMUSH_URL . 'app/assets/images/graphic-smush-cdn-default@2x.png' ); ?> 2x"
-		alt="<?php esc_html_e( 'Smush CDN', 'wp-smushit' ); ?>">
+	<?php if ( ! $this->hide_wpmudev_branding() ) : ?>
+		<img src="<?php echo esc_url( WP_SMUSH_URL . 'app/assets/images/graphic-smush-cdn-default.png' ); ?>"
+			srcset="<?php echo esc_url( WP_SMUSH_URL . 'app/assets/images/graphic-smush-cdn-default@2x.png' ); ?> 2x"
+			alt="<?php esc_html_e( 'Smush CDN', 'wp-smushit' ); ?>">
+	<?php endif; ?>
 
 	<p>
 		<?php
