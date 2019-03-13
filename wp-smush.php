05789f75521c268cce6c7196b37fbb5778c5be13
Anton Vanyukov <anton@incsub.com>
2019-03-13 11:30:58 +0300

    Update version

--- wp-smush.php
+++ wp-smush.php
@@ -49,7 +49,7 @@ if ( ! defined( 'WPINC' ) ) {
 }
 
 if ( ! defined( 'WP_SMUSH_VERSION' ) ) {
-	define( 'WP_SMUSH_VERSION', '3.2.0.1' );
+	define( 'WP_SMUSH_VERSION', '3.2.0.1-beta.1' );
 }
 // Used to define body class.
 if ( ! defined( 'WP_SHARED_UI_VERSION' ) ) {
