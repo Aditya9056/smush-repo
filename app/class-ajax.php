2537004be533f431ee3ba4e8045a0624da26c39b
Anton Vanyukov <anton@incsub.com>
2019-10-10 17:21:31 +1000

    Fix: Re-check images notification not dismissing

--- app/class-ajax.php
+++ app/class-ajax.php
@@ -410,7 +410,7 @@ class Ajax {
 						<p>' . $message . '</p>
 					</div>
 					<span class="sui-notice-dismiss">
-						<a role="button" href="#" aria-label="' . __( 'Dismiss', 'wp-smushit' ) . '" class="sui-icon-check"></a>
+						<a role="button" href="#" aria-label="' . __( 'Dismiss', 'wp-smushit' ) . '" class="sui-icon-check" id="bulk-smush-top-notice-close"></a>
 					</span>
 				</div>';
 
