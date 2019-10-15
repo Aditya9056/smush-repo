cb580f0eced0c2d16926bab2fe97bc401afe2789
Anton Vanyukov <anton@incsub.com>
2019-10-15 11:35:52 +1000

    Rever last change

--- core/modules/class-cdn.php
+++ core/modules/class-cdn.php
@@ -683,7 +683,7 @@ class CDN extends Abstract_Module {
 		// Get maximum content width.
 		$content_width = $this->max_content_width();
 
-		if ( is_array( $size ) && $size[0] > $content_width ) {
+		if ( is_array( $size ) && $size[0] < $content_width ) {
 			return $sizes;
 		}
 
@@ -1040,12 +1040,6 @@ class CDN extends Abstract_Module {
 		foreach ( $additional_multipliers as $multiplier ) {
 			// New width by multiplying with original size.
 			$new_width = intval( $base_width * $multiplier );
-
-			// In most cases - going over the current width is not recommended and probably not what the user is expecting.
-			if ( $new_width > $current_width ) {
-				continue;
-			}
-
 			// If a nearly sized image already exist, skip.
 			foreach ( $current_widths as $_width ) {
 				// If +- 50 pixel difference - skip.
