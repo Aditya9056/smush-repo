838fc1484e95944f72c8e5023029cf3f473a1851
Anton Vanyukov <anton@incsub.com>
2019-12-24 10:04:41 +1000

    Update packages. Update SUI. Update lazysizes library

--- wp-smush.php
+++ wp-smush.php
@@ -57,7 +57,7 @@ if ( ! defined( 'WP_SMUSH_VERSION' ) ) {
 }
 // Used to define body class.
 if ( ! defined( 'WP_SHARED_UI_VERSION' ) ) {
-	define( 'WP_SHARED_UI_VERSION', 'sui-2-5-2' );
+	define( 'WP_SHARED_UI_VERSION', 'sui-2-6-0' );
 }
 if ( ! defined( 'WP_SMUSH_BASENAME' ) ) {
 	define( 'WP_SMUSH_BASENAME', plugin_basename( __FILE__ ) );
