5bb567aa2d17edc4d9a201c9c2f7333b05f28169
Anton Vanyukov <anton@incsub.com>
2020-01-13 16:20:35 +1000

    Adjust progress bar translation strings

---
+++ _src/js/frontend/lazy-load.js
@@ -0,0 +1,17 @@
+window.lazySizesConfig = window.lazySizesConfig || {};
+
+window.lazySizesConfig.lazyClass = 'lazyload';
+window.lazySizesConfig.loadingClass = 'lazyloading';
+window.lazySizesConfig.loadedClass = 'lazyloaded';
+
+window.lazySizesConfig.loadMode = 1;
+
+import lazySizes from 'lazysizes';
+import 'lazysizes/plugins/native-loading/ls.native-loading';
+
+lazySizes.cfg.nativeLoading = {
+	setLoadingAttribute: true,
+	disableListeners: {
+		scroll: true,
+	},
+};
