dbc3a450da27604df21d7e5e843dabd87342a8dc
Anton Vanyukov <anton@incsub.com>
2020-01-14 08:59:32 +1000

    Adjust lazysizes config

--- _src/js/frontend/lazy-load.js
+++ _src/js/frontend/lazy-load.js
@@ -1,11 +1,3 @@
-window.lazySizesConfig = window.lazySizesConfig || {};
-
-window.lazySizesConfig.lazyClass = 'lazyload';
-window.lazySizesConfig.loadingClass = 'lazyloading';
-window.lazySizesConfig.loadedClass = 'lazyloaded';
-
-window.lazySizesConfig.loadMode = 1;
-
 import lazySizes from 'lazysizes';
 import 'lazysizes/plugins/native-loading/ls.native-loading';
 
