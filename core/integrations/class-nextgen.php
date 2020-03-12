0cb6d2462f3f8f9ad671d3cd91f80fc20bbc7817
Anton Vanyukov <anton@incsub.com>
2020-03-12 14:57:19 +1000

    Update composer, nextgen and gutenberg notices

--- core/integrations/class-nextgen.php
+++ core/integrations/class-nextgen.php
@@ -254,15 +254,18 @@ class NextGen extends Abstract_Integration {
 	 * @param string $name  Setting name.
 	 */
 	public function additional_notice( $name ) {
-		if ( 'nextgen' === $name && ! $this->enabled ) {
+		if ( $this->module === $name && ! $this->enabled ) {
 			?>
-            <div class="sui-toggle-content">
-                <div class="sui-notice sui-notice-sm">
-                    <p>
-                        <?php esc_html_e( 'To use this feature you need to be using NextGen Gallery.', 'wp-smushit' ); ?>
-                    </p>
-                </div>
-            </div>
+			<div class="sui-toggle-content">
+				<div class="sui-notice">
+					<div class="sui-notice-content">
+						<div class="sui-notice-message">
+							<i class="sui-notice-icon sui-icon-info sui-md" aria-hidden="true"></i>
+							<p><?php esc_html_e( 'To use this feature you need to be using NextGen Gallery.', 'wp-smushit' ); ?></p>
+						</div>
+					</div>
+				</div>
+			</div>
 			<?php
 		}
 	}
